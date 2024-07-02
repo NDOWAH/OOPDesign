f620b69078ba9d859e7f5288229300673a5ed6f0
Marcel grossmann <marcelgrossmann80@gmail.com>
2024-07-02 04:41:01 +0100

    print requested number of list items

---
+++ OOP_design/maxsize.py
@@ -0,0 +1,36 @@
+class MaxSizeList(object):
+    """takes an integer and return a list with spicified
+    number of integers
+    params:
+    list_size: max list size to return"""
+    word_list = []
+
+    def __init__(self, list_size):
+        self.list_size = list_size
+
+    def push(self, word):
+        """insert words into the class word_list
+        param
+        ======
+        word: word to be inserted into word word_list"""
+        MaxSizeList.word_list.append(word)
+
+    def get_list(self):
+        """Returns list of inserted words of size as list_size(if list_size is greater than
+        word_list functions returns list chopping of the first few words
+        """
+        return self.word_list[(len(self.word_list)-self.list_size):]
+
+
+a = MaxSizeList(3)
+a.push('hey')
+a.push('hi')
+a.push('let\'s')
+a.push('go')
+print(a.get_list())
+b = MaxSizeList(1)
+b.push('hi')
+b.push('hey')
+b.push('let\'s')
+b.push('go')
+print(b.get_list())
